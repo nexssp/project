df9fb398ea0eec10c966761afd2096e277d2f566
Marcin Polak <mapoart@gmail.com>
2021-11-15 19:01:43 +0100

    added vue3 as projectType.

--- src/projectTypes.js
+++ src/projectTypes.js
@@ -1,4 +1,5 @@
 module.exports = {
+  vue3: { repo: 'https://github.com/nexssp/project_vue3.git' },
   vue2: { repo: 'https://github.com/nexssp/project_vue2.git' },
   react17: { repo: 'https://github.com/nexssp/project_react17.git' },
   preact: { repo: 'https://github.com/nexssp/project_preact.git' },
